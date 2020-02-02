ba699bf2190c26d3c83171aa4d1f03a7a66d5483
kaishu16 <barcelona_df_5@yahoo.com>
2020-02-02 22:13:46 +0900

    line talk

--- index.js
+++ index.js
@@ -36,38 +36,7 @@ function lineBot(req, res) {
 function echoman(ev) {
   // const pro =  await client.getProfile(ev.source.userId);
   return client.replyMessage(ev.replyToken, {
-  "type": "template",
-  "altText": "This is a buttons template",
-  "template": {
-      "type": "buttons",
-      "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
-      "imageAspectRatio": "rectangle",
-      "imageSize": "cover",
-      "imageBackgroundColor": "#FFFFFF",
-      "title": "Menu",
-      "text": "Please select",
-      "defaultAction": {
-          "type": "uri",
-          "label": "View detail",
-          "uri": "http://example.com/page/123"
-      },
-      "actions": [
-          {
-            "type": "postback",
-            "label": "Buy",
-            "data": "action=buy&itemid=123"
-          },
-          {
-            "type": "postback",
-            "label": "Add to cart",
-            "data": "action=add&itemid=123"
-          },
-          {
-            "type": "uri",
-            "label": "View detail",
-            "uri": "http://example.com/page/123"
-          }
-      ]
-  }
-})
+    type: "text",
+    text: `今「${ev.message.text}」って言いました？`
+  })
 }
