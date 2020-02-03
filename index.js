83dea0356d55878d77eee5c8fc7d6ba43f7766ea
kaishu16 <barcelona_df_5@yahoo.com>
2020-02-03 12:56:44 +0900

    初期メッセージ テスト

--- index.js
+++ index.js
@@ -52,10 +52,10 @@ async function echoman(ev) {
   if (userMessage == "振り返り"){
     let reply = '';
     reply = `${pro.displayName}さんお疲れ様です！今日も学習を振り返っていきましょう！`;
-    // client.pushMessage(ev.source.userId, {
-    //     type: 'text',
-    //     text: `本日も予定通り学習できましたか？`,
-    // });
+    client.pushMessage(ev.source.userId, {
+        type: 'text',
+        text: `本日も予定通り学習できましたか？`,
+    });
     return client.replyMessage(ev.replyToken, {
       type: "text",
       text: reply
