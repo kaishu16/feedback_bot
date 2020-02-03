1375b28a35a69eaca24e56114b958a1bb61a067c
kaishu16 <barcelona_df_5@yahoo.com>
2020-02-03 21:58:48 +0900

    新運用 テスト

--- index.js
+++ index.js
@@ -23,21 +23,19 @@ function lineBot(req, res) {
   // ここから追加
   const events = req.body.events;
   const promises = [];
-
-  events.forEach((event) =>{
-      promises.push(client.replyMessage(event.replyToken, {
-        type: "text",
-        text: rep(event)
-      }));
-  });
-
+  for (let i = 0, l = events.length; i < l; i++) {
+    const ev = events[i];
+    promises.push(
+      echoman(ev)
+    );
+  }
   Promise.all(promises).then(console.log("pass"));
 
 
 }
 
 // 追加
-async function rep(ev) {
+async function echoman(ev) {
 
   const userMessage = ev.message.text;
 
@@ -54,9 +52,21 @@ async function rep(ev) {
 
   if (userMessage == "振り返り"){
     reply = `${pro.displayName}さんお疲れ様です！今日も学習を振り返っていきましょう！`;
+    // answer = first_question(ev.source.userId);
+    // userMessage = answer;
+
+    // setTimeout(() => {
+    //   client.pushMessage(ev.source.userId, {
+    //       type: 'text',
+    //       text: `また分からない点や悩んでいることがあったら連絡ください！頑張っていきましょうー！`,
+    //   });
+    // },2000);
   }
 
-  return reply;
+  return client.replyMessage(ev.replyToken, {
+    type: "text",
+    text: reply
+  });
 }
 
 // function first_question(userId) {
@@ -89,8 +99,8 @@ async function rep(ev) {
 //
 // }
 //
-// function second_question(userId, ans) {
-//   if (ans == "はい"){
+// function second_question(userId, answer) {
+//   if (answer == "yes"){
 //   let yes_question =
 //   {
 //     "type": "template",
@@ -120,7 +130,7 @@ async function rep(ev) {
 //
 //   };
 //
-//   if (ans == "いいえ"){
+//   if (answer == "no"){
 //   let no_question =
 //   {
 //     "type": "template",
@@ -150,126 +160,126 @@ async function rep(ev) {
 //   };
 //
 // }
-// //
-// // function third_question(userId, answer) {
-// //   if (answer == "yes"){
-// //   let yes_question =
-// //   {
-// //     "type": "template",
-// //     "altText": "3個目の質問",
-// //     "template": {
-// //         "type": "confirm",
-// //         "text": "なるほど！より質の高い学習をするにはどうすれば良いでしょうか？",
-// //         "actions": [
-// //             {
-// //                 "type": "message",
-// //                 "label": "はい",
-// //                 "text": "はい"
-// //             },
-// //             {
-// //                 "type": "message",
-// //                 "label": "いいえ",
-// //                 "text": "いいえ"
-// //             }
-// //         ]
-// //     }
-// //   };
-// //   setTimeout(() => {
-// //     client.pushMessage(userId, yes_question);
-// //   },2000);
-// //   };
-// //
-// //   if (answer == "no"){
-// //   let no_question =
-// //   {
-// //     "type": "template",
-// //     "altText": "3個目の質問",
-// //     "template": {
-// //         "type": "confirm",
-// //         "text": "なるほど！予定通り進まなかった要因として何があげられますか？",
-// //         "actions": [
-// //             {
-// //                 "type": "message",
-// //                 "label": "はい",
-// //                 "text": "はい"
-// //             },
-// //             {
-// //                 "type": "message",
-// //                 "label": "いいえ",
-// //                 "text": "いいえ"
-// //             }
-// //         ]
-// //     }
-// //   };
-// //   setTimeout(() => {
-// //     client.pushMessage(userId, no_question);
-// //   },2000);
-// //   };
-// //
-// // }
-// //
-// //
-// // function last_question(userId, answer) {
-// //   let default = {
-// //     type: 'text',
-// //     text: `それではスケジュールノートをもとに明日の学習計画を見直していきましょう！`,
-// //   }
-// //   setTimeout(() => {
-// //     client.pushMessage(userId, default);
-// //   },2000);
-// //
-// //   if (answer == "yes"){
-// //   let yes_question =
-// //   {
-// //     "type": "template",
-// //     "altText": "最後の質問",
-// //     "template": {
-// //         "type": "confirm",
-// //         "text": "明日の学習計画は変更しなくても良さそうですか？",
-// //         "actions": [
-// //             {
-// //                 "type": "message",
-// //                 "label": "はい",
-// //                 "text": "はい"
-// //             },
-// //             {
-// //                 "type": "message",
-// //                 "label": "いいえ",
-// //                 "text": "いいえ"
-// //             }
-// //         ]
-// //     }
-// //   };
-// //   setTimeout(() => {
-// //     client.pushMessage(userId, yes_question);
-// //   },2000);
-// //   };
-// //
-// //   if (answer == "no")
-// //   let no_question =
-// //   {
-// //     "type": "template",
-// //     "altText": "最後の質問",
-// //     "template": {
-// //         "type": "confirm",
-// //         "text": "明日の学習計画は変更した方が良さそうですか？",
-// //         "actions": [
-// //             {
-// //                 "type": "message",
-// //                 "label": "はい",
-// //                 "text": "はい"
-// //             },
-// //             {
-// //                 "type": "message",
-// //                 "label": "いいえ",
-// //                 "text": "いいえ"
-// //             }
-// //         ]
-// //     }
-// //   };
-// //   setTimeout(() => {
-// //     client.pushMessage(userId, no_question);
-// //   },2000);
-// //   };
-// //
-// // }
+//
+// function third_question(userId, answer) {
+//   if (answer == "yes"){
+//   let yes_question =
+//   {
+//     "type": "template",
+//     "altText": "3個目の質問",
+//     "template": {
+//         "type": "confirm",
+//         "text": "なるほど！より質の高い学習をするにはどうすれば良いでしょうか？",
+//         "actions": [
+//             {
+//                 "type": "message",
+//                 "label": "はい",
+//                 "text": "はい"
+//             },
+//             {
+//                 "type": "message",
+//                 "label": "いいえ",
+//                 "text": "いいえ"
+//             }
+//         ]
+//     }
+//   };
+//   setTimeout(() => {
+//     client.pushMessage(userId, yes_question);
+//   },2000);
+//   };
+//
+//   if (answer == "no"){
+//   let no_question =
+//   {
+//     "type": "template",
+//     "altText": "3個目の質問",
+//     "template": {
+//         "type": "confirm",
+//         "text": "なるほど！予定通り進まなかった要因として何があげられますか？",
+//         "actions": [
+//             {
+//                 "type": "message",
+//                 "label": "はい",
+//                 "text": "はい"
+//             },
+//             {
+//                 "type": "message",
+//                 "label": "いいえ",
+//                 "text": "いいえ"
+//             }
+//         ]
+//     }
+//   };
+//   setTimeout(() => {
+//     client.pushMessage(userId, no_question);
+//   },2000);
+//   };
+//
+// }
+//
+//
+// function last_question(userId, answer) {
+//   let default = {
+//     type: 'text',
+//     text: `それではスケジュールノートをもとに明日の学習計画を見直していきましょう！`,
+//   }
+//   setTimeout(() => {
+//     client.pushMessage(userId, default);
+//   },2000);
+//
+//   if (answer == "yes"){
+//   let yes_question =
+//   {
+//     "type": "template",
+//     "altText": "最後の質問",
+//     "template": {
+//         "type": "confirm",
+//         "text": "明日の学習計画は変更しなくても良さそうですか？",
+//         "actions": [
+//             {
+//                 "type": "message",
+//                 "label": "はい",
+//                 "text": "はい"
+//             },
+//             {
+//                 "type": "message",
+//                 "label": "いいえ",
+//                 "text": "いいえ"
+//             }
+//         ]
+//     }
+//   };
+//   setTimeout(() => {
+//     client.pushMessage(userId, yes_question);
+//   },2000);
+//   };
+//
+//   if (answer == "no")
+//   let no_question =
+//   {
+//     "type": "template",
+//     "altText": "最後の質問",
+//     "template": {
+//         "type": "confirm",
+//         "text": "明日の学習計画は変更した方が良さそうですか？",
+//         "actions": [
+//             {
+//                 "type": "message",
+//                 "label": "はい",
+//                 "text": "はい"
+//             },
+//             {
+//                 "type": "message",
+//                 "label": "いいえ",
+//                 "text": "いいえ"
+//             }
+//         ]
+//     }
+//   };
+//   setTimeout(() => {
+//     client.pushMessage(userId, no_question);
+//   },2000);
+//   };
+//
+// }
