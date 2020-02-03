7b7076c77aaa9cb8c1f5d849ba771d2d72943f0d
kaishu16 <barcelona_df_5@yahoo.com>
2020-02-03 17:35:56 +0900

    質問 テスト

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ async function echoman(ev) {
   if (userMessage == "振り返り"){
     let reply = '';
     reply = `${pro.displayName}さんお疲れ様です！今日も学習を振り返っていきましょう！`;
-    first_question(ev.source.userId, userMessage);
+    first_question(ev.source.userId);
     setTimeout(() => {
       client.pushMessage(ev.source.userId, {
           type: 'text',
