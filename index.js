64a1774ba71a02683fe668a754db04859e275554
kaishu16 <barcelona_df_5@yahoo.com>
2020-02-03 13:04:14 +0900

    初期メッセージ テスト

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ async function echoman(ev) {
   if (userMessage == "振り返り"){
     let reply = '';
     reply = `${pro.displayName}さんお疲れ様です！今日も学習を振り返っていきましょう！`;
-    client.pushMessage(ev.source.userId, {
+    await client.pushMessage(ev.source.userId, {
         type: 'text',
         text: `本日も予定通り学習できましたか？`,
     });
