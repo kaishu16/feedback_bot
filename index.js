92f1a3a0b9f8e5045f1d617920eb26983d7f62bf
kaishu16 <barcelona_df_5@yahoo.com>
2020-02-09 14:38:31 +0900

    テスト

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ function lineBot(req, res) {
         cause = 'last_question_yes'
         promises.push(
           getQuestion3_Yes2_Obj(event, jsonFile).then(function(){
-            getLastQuestionYesObj(event, jsonFile)
+            return getLastQuestionYesObj(event, jsonFile);
           })
         console.log(cause);
       }
