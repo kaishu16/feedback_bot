6cbed5d99a89cef7b9c189b621c0b435c7237e22
kaishu16 <barcelona_df_5@yahoo.com>
2020-02-10 17:27:42 +0900

    テスト

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ async function getAnswerObj(data, jsonFile){
                   let question = JSON.parse(message2);
                   send.text = pro.displayName + send.text
                   setTimeout(() => {client.pushMessage(data.source.userId, question)}, 1500);
-                  return client.replyMessage(data.replyToken, send);
+                  return setTimeout(() => {client.pushMessage(data.source.userId, send)}, 6000);
               }
         case 'postback':
             console.log('postbackの場合');
